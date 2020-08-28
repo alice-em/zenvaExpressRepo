80ca4576e10cd802c27eba6f319fd218f67eff69
alice maldonado <the.alice.maldonado@gmail.com>
2020-08-28 16:14:11 -0500

    chapter 2

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('sup doggie');
+  res.render('home', null);
 });
 
 module.exports = router;
